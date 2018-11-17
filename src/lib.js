c73b984c3afb5f9f6f9716e6c9d39ede29bc3a47
Pierre Romera <hello@pirhoo.com>
2018-11-17 21:33:33 +0100

    Export all components from the root of the lib

--- src/lib.js
+++ src/lib.js
@@ -7,7 +7,18 @@ import GenericFooter from './components/GenericFooter.vue'
 import MainHeader from './components/MainHeader.vue'
 import SharingOptions from './components/SharingOptions.vue'
 import SignUpForm from './components/SignUpForm.vue'
+
 import config from './config'
+
+export { default as ContentPlaceholder } from './components/ContentPlaceholder.vue'
+export { default as DonateForm } from './components/DonateForm.vue'
+export { default as EmbedableFooter } from './components/EmbedableFooter.vue'
+export { default as EmbedForm } from './components/EmbedForm.vue'
+export { default as FollowUsPopover } from './components/FollowUsPopover.vue'
+export { default as GenericFooter } from './components/GenericFooter.vue'
+export { default as MainHeader } from './components/MainHeader.vue'
+export { default as SharingOptions } from './components/SharingOptions.vue'
+export { default as SignUpForm } from './components/SignUpForm.vue'
 
 export const components = {
   ContentPlaceholder,
