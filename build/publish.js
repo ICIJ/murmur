b49dc7082ccaa1f97b45c90c9f5a7215b5ee92d7
Pierre Romera <hello@pirhoo.com>
2018-11-17 22:12:18 +0100

    Added publish command

---
+++ build/publish.js
@@ -0,0 +1,16 @@
+require('./check-versions')()
+
+const gh = require('gh-pages');
+const path = require('path');
+const ora = require('ora')
+const chalk = require('chalk')
+
+process.env.NODE_ENV = 'production'
+
+const spinner = ora('push dist folder to gh-pages branch...')
+spinner.start()
+
+gh.publish(path.join(process.cwd(), 'dist'), () => {
+  spinner.stop()
+  console.log(chalk.cyan('  Push complete.\n'))
+});
