2462d4ef4b199abcd2e76e95fce14e049e27df74
Pierre Romera <hello@pirhoo.com>
2018-11-24 19:40:49 +0100

    Fixed url

--- docs/components/responsive-iframe/sample.vue
+++ docs/components/responsive-iframe/sample.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="card m-4">
-    <responsive-iframe url="http://blog.apps.npr.org/pym.js/examples/table/child.html"></responsive-iframe>
+    <responsive-iframe url="https://blog.apps.npr.org/pym.js/examples/table/child.html"></responsive-iframe>
   </div>
 </template>
 
