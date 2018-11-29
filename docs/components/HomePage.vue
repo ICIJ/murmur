f28c4ab96911b3c84e3cb78f071374535c941b7c
Pierre Romera <hello@pirhoo.com>
2018-11-29 19:25:28 +0100

    Fixed slideupdown

--- docs/components/HomePage.vue
+++ docs/components/HomePage.vue
@@ -1,5 +1,11 @@
 <template>
-
+  <p class="lead">
+    The <a href="https://icij.org">International Consortium of Investigative Journalists</a>
+    aims to build efficient and innovative tools for the biggest cross-border investigations in the world.
+    In order to build stable, consistent and user-friendly webapps, our team
+    started to define common components between projects, as well as best practices and
+    guidelines.
+  </p>
 </template>
 
 <script>
