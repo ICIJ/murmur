53f60035aa17a00e18a5b51c6ac85fed5b57c660
Pierre Romera <hello@pirhoo.com>
2019-01-04 13:51:04 +0100

    Added tests for FollowUsPopover

---
+++ tests/unit/components/Fa.spec.js
@@ -0,0 +1,18 @@
+import { faTimes } from '@fortawesome/free-solid-svg-icons/faTimes'
+import { library, default as Fa } from '@/components/Fa'
+
+describe('Fa', () => {
+
+  it('is a FontAwesomeIcon component', () => {
+    expect(Fa.name).toBe('FontAwesomeIcon')
+  })
+
+  it('has a library that define a `add` method', () => {
+    expect(library.add).toBeDefined()
+  })
+
+  it('has a library that `add` method', () => {
+    library.add(faTimes)
+    expect(library.definitions.fas.times).toBeDefined()
+  })
+})
---
+++ tests/unit/components/FollowUsPopover.spec.js
@@ -0,0 +1,21 @@
+import { mount, shallowMount } from '@vue/test-utils'
+import FollowUsPopover from '@/components/FollowUsPopover.vue'
+
+describe('FollowUsPopover.vue', () => {
+
+  it('is a Vue instance', () => {
+    const wrapper = mount(FollowUsPopover)
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  it('has a sign-up form', () => {
+    const wrapper = shallowMount(FollowUsPopover)
+    expect(wrapper.find('sign-up-form-stub').exists()).toBeTruthy()
+  })
+
+  it('emits an event when clicking on close button', () => {
+    const wrapper = shallowMount(FollowUsPopover)
+    expect(wrapper.find('.follow-us__close').exists()).toBeTruthy()
+  })
+
+})
