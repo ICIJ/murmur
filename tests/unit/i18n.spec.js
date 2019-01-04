ba0008273b01963c946715db186747c67a4692a1
Pierre Romera <hello@pirhoo.com>
2019-01-04 22:23:23 +0100

    Added test for i18n

---
+++ tests/unit/i18n.spec.js
@@ -0,0 +1,16 @@
+import { locale, fallbackLocale, default as i18n } from '@/i18n'
+import VueI18n from 'vue-i18n'
+
+describe('i18n.js', () => {
+  it('exposes a static method called `install`', () => {
+    expect(i18n).toBeInstanceOf(VueI18n)
+  })
+
+  it('should use `en` as default locale', () => {
+    expect(locale).toBe('en')
+  })
+
+  it('should use `en` as default fallback locale', () => {
+    expect(fallbackLocale).toBe('en')
+  })
+})
