a5f3d03ebe2c34ee2e4da915d5843a8ca1cf377b
Pierre Romera <hello@pirhoo.com>
2019-01-04 16:46:28 +0100

    Added test for slideupdown component

--- tests/unit/components/SlideUpDown.spec.js
+++ tests/unit/components/SlideUpDown.spec.js
@@ -8,4 +8,41 @@ describe('SlideUpDown', () => {
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 
+  it('renders the component as a `div` by default', () => {
+    const wrapper = mount(SlideUpDown)
+    expect(wrapper.element.nodeName).toBe('DIV')
+  })
+
+  it('renders the component as a `span` when props.tag is passed', () => {
+    const tag = 'SPAN'
+    const wrapper = mount(SlideUpDown, {
+      propsData: { tag }
+    })
+    expect(wrapper.element.nodeName).toBe(tag)
+  })
+
+  it('is visible by default', () => {
+    const wrapper = mount(SlideUpDown)
+    expect(wrapper.element.style.height).toBe('auto')
+  })
+
+  it('isn\t visible if props.active is passed as `false`', () => {
+    const active = false
+    const wrapper = mount(SlideUpDown, {
+      propsData: { active }
+    })
+    wrapper.vm.mounted = true
+    expect(wrapper.element.style.height).toBe('0px')
+  })
+
+  it('calls `cleanLayout` after the transition', (done) => {
+    const wrapper = mount(SlideUpDown)
+    wrapper.vm.cleanLayout = jest.fn()
+    expect(wrapper.vm.cleanLayout.mock.calls.length).toBe(0)
+    wrapper.vm.active = false
+    setTimeout(() => {
+      expect(wrapper.vm.cleanLayout.mock.calls.length).toBe(1)
+      done()
+    }, wrapper.vm.duration)
+  })
 })
