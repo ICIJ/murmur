5d366b1c04122cb012e405a8c60dd675da4a4bb5
Pierre Romera <hello@pirhoo.com>
2019-02-20 14:01:29 +0100

    Restored setup

--- tests/unit/setup.js
+++ tests/unit/setup.js
@@ -3,5 +3,5 @@ const exec = util.promisify(require('child_process').exec);
 
 module.exports = async () => {
   // We build the lib to test it is well integrated
-  // return exec('yarn run build:lib')
+  return exec('yarn run build:lib')
 }
