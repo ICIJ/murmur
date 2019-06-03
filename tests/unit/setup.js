1e95d046314c8758596eedcfa68f73b712d7b2d1
Pierre Romera <hello@pirhoo.com>
2019-06-03 09:50:49 +0200

    Upgraded more packages

--- tests/unit/setup.js
+++ tests/unit/setup.js
@@ -1,5 +1,5 @@
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
 
 module.exports = async () => {
   // We build the lib to test it is well integrated
