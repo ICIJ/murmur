8bdc649729e076e39978f3a19811ab9524e6f7f3
Anne L'Hôte <anne.lhote@gmail.com>
2020-08-24 18:30:55 +0200

    Remove text-decoration on Generic Footer

--- lib/components/GenericFooter.vue
+++ lib/components/GenericFooter.vue
@@ -4,7 +4,7 @@
       <div class="row">
         <div class="col-md-5">
           <h5 class="text-uppercase text-white clearfix generic-footer__icij mb-3">
-            <a href="https://icij.org" target="_blank" class="generic-footer__icij__logo">
+            <a href="https://icij.org" target="_blank" class="generic-footer__icij__logo text-decoration-none">
               <img src="@/assets/images/icij-full-white.svg" alt="International Consortium of Investigative Journalists" />
             </a>
           </h5>
