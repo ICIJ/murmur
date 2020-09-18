64b003c58c9ebec59504a40c31614584a02a95c5
Pierre Romera <hello@pirhoo.com>
2020-09-18 13:45:31 +0200

    fix: typo in doc

--- lib/datavisualisations/LineChart.vue
+++ lib/datavisualisations/LineChart.vue
@@ -61,7 +61,7 @@ export default {
       default: null
     },
     /**
-     * Function to apply to format x axis ticks (bar value). It can be a
+     * Function to apply to format y axis ticks (line value). It can be a
      * function returning the formatted value or a d3's formatter string.
      */
     yAxisTickFormat: {
