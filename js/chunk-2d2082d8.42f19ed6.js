24229c6924173d2cd181bbef13eabcb15c08bdbc
Pierre Romera <hello@pirhoo.com>
2021-05-14 15:49:53 +0200

    Updates [ci skip]

---
+++ js/chunk-2d2082d8.42f19ed6.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2082d8"],{a44c:function(e,o){e.exports={displayName:"GenericFooter",description:"GenericFooter",tags:{},exportName:"default",props:[{name:"version",description:"Version of the app to display in the bottom-right corner of the footer",type:{name:"string"}}],slots:[{name:"left",description:"Additional content on the left side of the footer"},{name:"investigations",description:"List of investigations"}]}}}]);
+//# sourceMappingURL=chunk-2d2082d8.42f19ed6.js.map
--- js/utilities-assets-doc.1f89ad58.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["utilities-assets-doc"],{"0602":function(e,t,n){"use strict";n.r(t);var s=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div")},i=[],l={},u=l,c=n("2877"),a=Object(c["a"])(u,s,i,!1,null,null,null);t["default"]=a.exports}}]);
-//# sourceMappingURL=utilities-assets-doc.1f89ad58.js.map
