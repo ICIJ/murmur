e4408cb6d0f69958a985c1faa9dff8a0538e0e02
Pierre Romera <hello@pirhoo.com>
2022-10-19 16:32:45 +0000

    refactor: convert clipboard.js to typescript

---
+++ lib/utils/clipboard.ts
@@ -0,0 +1,41 @@
+import Clipboard from 'clipboard'
+
+export function copyText (text: string, container: HTMLElement) {
+  return new Promise(function (resolve: (value?: unknown) => void, reject: (value?: unknown) => void) {
+    const fakeElement = document.createElement('button')
+    // Use the document body as container is no valid object is provided
+    container = typeof container === 'object' ? container : document.body
+    
+    const clipboard: Clipboard = new Clipboard(fakeElement, { text: () => text, container })
+
+    clipboard.on('success', () => {
+      clipboard.destroy()
+      resolve()
+    })
+
+    clipboard.on('error', error => {
+      clipboard.destroy()
+      reject(error)
+    })
+
+    fakeElement.click()
+  })
+}
+
+export function copyHtml (html: string, plain: string) {
+  interface ClipboardEvent extends Event {
+    clipboardData: {
+      setData: (attribute: string, value: string) => void 
+    }
+  }
+
+  function listener(event: ClipboardEvent) {
+    event.clipboardData.setData("text/html", html)
+    event.clipboardData.setData("text/plain", plain)
+    event.preventDefault()
+  }
+  
+  document.addEventListener("copy", listener as EventListener)
+  document.execCommand("copy")
+  document.removeEventListener("copy", listener as EventListener)
+}
