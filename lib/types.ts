3f91c319ec8cc7d33f21d851b61b2b703ee36ac2
Caroline Desprat <cdesprat@icij.org>
2022-11-15 10:14:24 +0000

    fix: add definecomponent and lint Co-authored-by: Pierre Romera <pirhoo@users.noreply.github.com> Co-authored-by: Maxime Vanza Lutonda <mvanzalu@users.noreply.github.com> Co-authored-by: Bruno Thomas <bamthomas@users.noreply.github.com> Co-authored-by: marieglr <marieglr@users.noreply.github.com>

--- lib/types.ts
+++ lib/types.ts
@@ -1,16 +1,16 @@
-import {AccordionKey} from "@/keys";
+import { AccordionKey } from "@/keys";
 
 export type Step = symbol | string
 
 export type Accordion = {
-    emitAccordionNextStepEvent:  () => void;
+    emitAccordionNextStepEvent: () => void;
     emitAccordionPreviousStepEvent: () => void;
-    isActiveStep: (step:Step) => boolean;
-    isPreviousStep: (step:Step) => boolean;
-    isFirstStep: (step:Step) => boolean;
-    isLastStep: (step:Step) => boolean;
-    step:Step;
-    steps:Step[] ;
+    isActiveStep: (step: Step) => boolean;
+    isPreviousStep: (step: Step) => boolean;
+    isFirstStep: (step: Step) => boolean;
+    isLastStep: (step: Step) => boolean;
+    step: Step;
+    steps: Step[];
 }
 export type AccordionProvide = {
     [AccordionKey]: Accordion
