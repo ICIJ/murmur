85387b3606d1a9a1f5ddc83d34cc1c3347759e21
Pierre Romera <hello@pirhoo.com>
2023-01-27 13:50:32 +0000

    refactor: update depracted highlight.js API

--- loaders/highlight-loader.js
+++ loaders/highlight-loader.js
@@ -4,8 +4,8 @@ const loaderUtils = require('loader-utils')
 module.exports = function highlightLoader(source) {
   this && this.cacheable && this.cacheable()
   const query = loaderUtils.parseQuery(this.query || '?')
-  const lang = query.lang
-  const code = hljs.getLanguage(lang) ? hljs.highlight(lang, source) : hljs.highlightAuto(source)
+  const language = query.lang
+  const code = hljs.getLanguage(language) ? hljs.highlight(source, { language }) : hljs.highlightAuto(source)
   const json = JSON.stringify(code.value)
     .replace(/\u2028/g, '\\u2028')
     .replace(/\u2029/g, '\\u2029');
