e874929974804437ab4d111bc69fd4c46e79d4f4
Pierre Romera <hello@pirhoo.com>
2023-01-30 12:37:30 +0100

    feat: add markdown-it plugins

--- docs/markdown/index.js
+++
@@ -1,9 +0,0 @@
-const markdown = require('@vuepress/markdown')
-const apiTable = require('./api-table')
-const sampleCard = require('./sample-card')
-
-module.exports = markdown()
-  // Add internal pluggins to vuepress configuration
-  .use(...apiTable('api-table'))
-  .use(...sampleCard('sample-card'))
-  .use(require('markdown-it-highlightjs'))
