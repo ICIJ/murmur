2aa3d317815769e8d0e761f8b0989c2c4026d653
Caroline Desprat <cdesprat@icij.org>
2023-04-06 15:25:23 +0000

    refactor: update GenericHeader as TS component

--- lib/shims-bootstrap-vue.d.ts
+++ lib/shims-bootstrap-vue.d.ts
@@ -1,1 +1,3 @@
 declare module 'bootstrap-vue/esm/components/tooltip/tooltip'
+declare module 'bootstrap-vue/esm/components/modal/modal'
+declare module 'bootstrap-vue/esm/components/popover/popover'
