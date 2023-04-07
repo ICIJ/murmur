54ea72d2956ca1697e4f4af3affb6538cc7bcfb6
Caroline Desprat <cdesprat@icij.org>
2023-04-07 13:06:15 +0000

    refactor: update OrdinalLegend as TS component and add D3 types

--- lib/components/OrdinalLegend.vue
+++ lib/components/OrdinalLegend.vue
@@ -1,12 +1,15 @@
-<script>
+<script lang='ts'>
 import { get, isFunction, kebabCase, uniqueId } from 'lodash'
 import * as d3 from 'd3'
-
-export default {
+import { defineComponent } from 'vue'
+import { PropType } from 'vue/types/v3-component-props'
+type Datum = {id: string, color:string, path:string, label:string }
+type Category = ('id'|'color'|'path'|'label' )
+export default defineComponent({
   name: 'OrdinalLegend',
   props: {
     data:{
-      type:Array,
+      type: Array as PropType<Datum[]>,
       default:()=>([])
     },
     horizontal:{
@@ -17,7 +20,7 @@ export default {
       default: 'M512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256z'
     },
     categoryObjectsPath: {
-      type: [String, Array],
+      type: String as PropType<Category>,
       default: 'id'
     },
     highlight: {
@@ -30,15 +33,15 @@ export default {
     }
   },
   computed:{
-    markerBoundingClientRect () {
+    markerBoundingClientRect (): DOMRect | undefined {
       const svg = d3.select("body").append("svg").attr('width', 2046).attr('height', 2046)
-      const marker = svg.append('path').attr('d', this.markerPathFunction)
-      const rect = marker.node().getBoundingClientRect()
+      const marker = svg.append('path').attr('d', this.markerPathFunction())
+      const rect: DOMRect | undefined = marker.node()?.getBoundingClientRect()
       svg.remove()
       return rect
     },
     markerViewbox () {
-      const { width, height } = this.markerBoundingClientRect
+      const { width, height } = this.markerBoundingClientRect ?? {width:0, height:0}
       return `0 0 ${width} ${height}`
     },
     classList(){
@@ -49,14 +52,14 @@ export default {
       }
     },
     dataWithIds () {
-      return this.data.map(d => {
+      return this.data.map((d:any):{[key:string]:string} => {
         const id = uniqueId()
         return { [this.categoryObjectsPath]: id, ...d }
       })
     }
   },
   methods:{
-    itemClassList (d) {
+    itemClassList (d:{[key:string]:string}) {
       const id = d[this.categoryObjectsPath]
       return {
         [`ordinal-legend__item--identifier-${kebabCase(d.label)}`]: true,
@@ -64,10 +67,10 @@ export default {
         'ordinal-legend__item--selected': id === this.value
       }
     },
-    markerPathFunction (d) {
-      return isFunction(this.markerPath) ? this.markerPath(d) : this.markerPath
+    markerPathFunction (d?: Datum): string {
+      return isFunction(this.markerPath) && d ? this.markerPath(d) : this.markerPath
     },
-    update (d) {
+    update (d:Datum) {
       /**
        * Fired when user clicks on an item
        * @event update 
@@ -84,7 +87,7 @@ export default {
       this.$emit('update:highlight', get(d, this.categoryObjectsPath, null))
     }
   }
-}
+})
 </script>
 
 <template>
